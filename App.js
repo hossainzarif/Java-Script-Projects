0ff4d82939e5deb8f9b6023bab3e75075f8eebbb
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-10-06 19:43:15 +0600

    commited

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import HomeScreen from "./src/screens/HomeScreen"
+import ListScreen from "./src/screens/ListScreen"
+import Semesters from "./src/screens/Semesters"
+import Courses from "./src/screens/Courses"
+import Faculties from "./src/screens/Faculties"
+import Profile from "./src/screens/Profile"
+const stack = createStackNavigator()
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <stack.Navigator initialRouteName ="Home">
+
+        <stack.Screen name="Home" component={HomeScreen} />
+        <stack.Screen name="List" component={ListScreen} />
+        <stack.Screen name="Semesters" component={Semesters} />
+        <stack.Screen name="Courses" component={Courses} />
+        <stack.Screen name="Faculties" component={Faculties} />
+        <stack.Screen name="Profile" component={Profile} />
+
+      </stack.Navigator>
+    </NavigationContainer>
+
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
